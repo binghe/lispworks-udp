1d3e193f915e1104a4074bdbb812232bee5754ff
Chun Tian <binghe.lisp@gmail.com>
2008-02-26 17:52:18 +0000

    New message-based API with test

--- lispworks-udp.lisp
+++ lispworks-udp.lisp
@@ -8,14 +8,18 @@
 
 ;;; Exports into COMM Package
 (export '(;; UDP Client
-          open-udp-stream
+          open-udp-stream with-udp-stream
           connect-to-udp-server
+          open-udp-socket with-udp-socket
+          send-message
+          receive-message
           ;; UDP Server
-          start-udp-server
-          stop-udp-server
+          start-udp-server with-udp-server
           ;; UNIX Domain Socket
           open-unix-domain-stream
           connect-to-unix-domain-socket))
+
+(defparameter *max-udp-message-size* 65536)
 
 ;;;; Below is something we have to define (others already in COMM package)
 ;;;; binghe: my design goal is to use what COMM already have, and no C code.
@@ -23,6 +27,10 @@
 (defconstant *socket_sock_dgram* 2
   "Connectionless, unreliable datagrams of fixed maximum length.")
 (defconstant *sockopt_so_rcvtimeo* #x1006 "receive timeout")
+
+(fli:define-c-struct timeval
+  (tv-sec :long)
+  (tv-usec :long))
 
 ;;; ssize_t
 ;;; recvfrom(int socket, void *restrict buffer, size_t length, int flags,
@@ -48,3 +56,14 @@
      (address-len :int))
   :result-type :int)
 
+(defun set-socket-receive-timeout (socket-fd sec &optional (usec 0))
+  "Set socket option: RCVTIMEO"
+  (declare (type integer socket-fd sec usec))
+  (fli:with-dynamic-foreign-objects ((timeout (:struct timeval)))
+    (fli:with-foreign-slots (tv-sec tv-usec) timeout
+      (setf tv-sec sec tv-usec usec)
+      (setsockopt socket-fd
+                  *sockopt_sol_socket*
+                  *sockopt_so_rcvtimeo*
+                  timeout
+                  (fli:size-of '(:struct timeval))))))
--- udp-client.lisp
+++ udp-client.lisp
@@ -4,40 +4,93 @@
 
 (in-package :comm)
 
-(defun connect-to-udp-server (host service &key errorp local-address local-port)
-  "Something like CONNECT-TO-TCP-SERVER"
-  (let ((socket-fd (socket *socket_af_inet*
-			   *socket_sock_dgram*
-			   *socket_pf_unspec*)))
+(defun open-udp-socket (&key errorp local-address local-port read-timeout)
+  "Open a unconnected UDP socket"
+  (let ((socket-fd (socket *socket_af_inet* *socket_sock_dgram* *socket_pf_unspec*)))
     (if socket-fd
-      (fli:with-dynamic-foreign-objects ((server-addr sockaddr_in)
-                                         (client-addr sockaddr_in))
-        ;; bind to local address/port if specified.
-        (when (and local-address local-port)
-          (initialize-sockaddr_in client-addr *socket_af_inet* local-address local-port "udp")
-          (bind socket-fd
-                (fli:copy-pointer client-addr :type 'sockaddr)
-                (fli:pointer-element-size client-addr)))
+      (progn
+        (when read-timeout (set-socket-receive-timeout socket-fd read-timeout))
+        (if (and local-address local-port)
+          (progn ;; bind to local address/port if specified.
+            (fli:with-dynamic-foreign-objects ((client-addr (:struct sockaddr_in)))
+              (initialize-sockaddr_in client-addr *socket_af_inet* local-address local-port "udp")
+              (if (bind socket-fd
+                        (fli:copy-pointer client-addr :type '(:struct sockaddr))
+                        (fli:pointer-element-size client-addr))
+                ;; success, return socket fd
+                socket-fd
+                (progn ;; fail, close socket and return nil
+                  (close-socket socket-fd)
+                  (if errorp (error "cannot bind local address/port") nil)))))
+          socket-fd))
+      (if errorp (error "cannot create socket") nil))))
+
+(defmacro with-udp-socket ((socket &rest args) &body body)
+  `(let ((,socket (open-udp-socket ,@args)))
+     (unwind-protect
+         (progn ,@body)
+       (close-socket ,socket))))
+
+(defun send-message (socket host service data)
+  "Send message to a socket, using sendto()"
+  (declare (type (simple-array (unsigned-byte 8) (8)) data))
+  (let ((message (make-array *max-udp-message-size*
+                             :element-type '(unsigned-byte 8)
+                             :initial-element 0
+                             :allocation :static)))
+    (fli:with-dynamic-foreign-objects ((client-addr (:struct sockaddr_in))
+                                       (len :int
+                                            #+(and lispworks5 (not lispworks5.0))
+                                            :initial-element
+                                            (fli:size-of '(:struct sockaddr_in))))
+      (initialize-sockaddr_in client-addr *socket_af_inet* host service "udp")
+      (fli:with-dynamic-lisp-array-pointer (ptr message :type :unsigned-byte)
+        (replace message data)
+        (%sendto socket ptr (min (length data) *max-udp-message-size*) 0
+                 (fli:copy-pointer client-addr :type '(:struct sockaddr))
+                 (fli:dereference len))))))
+
+(defun receive-message (socket &key read-timeout)
+  "Receive message from socket"
+  (let ((message (make-array *max-udp-message-size*
+                             :element-type '(unsigned-byte 8)
+                             :initial-element 0
+                             :allocation :static)))
+    (fli:with-dynamic-foreign-objects ((client-addr (:struct sockaddr_in))
+                                       (len :int
+                                            #+(and lispworks5 (not lispworks5.0))
+                                            :initial-element
+                                            (fli:size-of '(:struct sockaddr_in))))
+      (fli:with-dynamic-lisp-array-pointer (ptr message :type :unsigned-byte)
+        (when read-timeout (set-socket-receive-timeout socket read-timeout))
+        (let ((n (%recvfrom socket ptr *max-udp-message-size* 0
+                            (fli:copy-pointer client-addr :type '(:struct sockaddr))
+                            len)))
+          (if (plusp n)
+            (subseq message 0 n)))))))
+
+(defun connect-to-udp-server (hostname service &key errorp local-address local-port)
+  "Something like CONNECT-TO-TCP-SERVER"
+  (let ((socket (open-udp-socket :errorp errorp
+                                    :local-address local-address
+                                    :local-port local-port)))
+    (if socket
+      (fli:with-dynamic-foreign-objects ((server-addr (:struct sockaddr_in)))
         ;; connect to remote address/port
-        (initialize-sockaddr_in server-addr *socket_af_inet* host service "udp")
-        (if (connect socket-fd
-                     (fli:copy-pointer server-addr :type 'sockaddr)
+        (initialize-sockaddr_in server-addr *socket_af_inet* hostname service "udp")
+        (if (connect socket
+                     (fli:copy-pointer server-addr :type '(:struct sockaddr))
                      (fli:pointer-element-size server-addr))
           ;; success, return socket fd
-          socket-fd
+          socket
           ;; fail, close socket and return nil
           (progn
-            (close-socket socket-fd)
+            (close-socket socket)
             (if errorp (error "cannot connect") nil))))
       (if errorp (error "cannot create socket") nil))))
 
-(defun open-udp-stream (hostname service &key
-                                 (direction :io)
-                                 (element-type 'base-char)
-                                 errorp
-                                 read-timeout
-                                 local-address
-                                 local-port)
+(defun open-udp-stream (hostname service &key (direction :io) (element-type 'base-char)
+                                 errorp  read-timeout local-address local-port)
   "Something like OPEN-TCP-STREAM"
   (let ((socket (connect-to-udp-server hostname service
                                        :errorp errorp
@@ -48,3 +101,9 @@
                    :direction direction
                    :element-type element-type
                    :read-timeout read-timeout)))
+
+(defmacro with-udp-stream ((stream &rest args) &body body)
+  `(let ((,stream (open-udp-stream ,@args)))
+     (unwind-protect
+         (progn ,@body)
+       (close ,stream))))
