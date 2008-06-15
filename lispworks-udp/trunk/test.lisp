845a812fc2b96efa8ecfbba64632a3a098630090
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-06-15 19:14:48 +0000

    [lispworks-udp] API change, better UDP server design. see ChangeLog

--- lispworks-udp/trunk/test.lisp
+++ lispworks-udp/trunk/test.lisp
@@ -2,12 +2,13 @@
 
 ;;; UDP Echo Test: use macros
 (defun udp-echo-test-3 (&optional (port 10000))
-  (let* ((fn #'(lambda (data host)
+  (let* ((fn #'(lambda (data)
                  (map '(simple-array (unsigned-byte 8) (*)) #'char-code
-                      (format nil "receive from ~A: ~A" host (map 'string #'code-char data)))))
+                      (format nil "receive from ~A:~A (~A)"
+                              *client-address* *client-port* (map 'string #'code-char data)))))
          (server-process (comm:start-udp-server :function fn :service port)))
     (unwind-protect
-        (comm:with-udp-stream (stream "localhost" port :read-timeout 1)
+        (comm:with-udp-stream (stream "localhost" port :read-timeout 1 :errorp t)
           (let ((data "Hello, world!"))
             (format stream "~A" data)
             (terpri stream) ;; = "~%" or #\Newline
@@ -18,12 +19,9 @@
       (comm:stop-udp-server server-process))))
 
 (defun udp-echo-test-4 (&optional (port 10000))
-  (let* ((echo-fn #'(lambda (data host)
-                      (declare (ignore host))
-                      data))
-         (server-process (comm:start-udp-server :function echo-fn :service port)))
+  (let* ((server-process (comm:start-udp-server :function #'reverse :service port)))
     (unwind-protect
-        (comm:with-udp-socket (socket :read-timeout 1)
+        (comm:with-udp-socket (socket :read-timeout 10)
           (let ((data #(1 2 3 4 5 6 7 8 9 10)))
             (comm:send-message socket data (length data) "localhost" port :max-buffer-size 8)
             (format t "SOCKET: Send message: ~A~%" data)
@@ -33,12 +31,11 @@
 
 (defun udp-echo-test-5 (&optional (port 10000))
   "Limit MAX-BUFFER-SIZE, less than send bytes."
-  (let* ((echo-fn #'(lambda (data host)
-                      (declare (ignore host))
+  (let* ((echo-fn #'(lambda (data)
                       data))
          (server-process (comm:start-udp-server :function echo-fn :service port)))
     (unwind-protect
-        (comm:with-connected-udp-socket (socket "localhost" port :read-timeout 1)
+        (comm:with-connected-udp-socket (socket "localhost" port :read-timeout 1 :errorp t)
           (let ((data #(1 2 3 4 5 6 7 8 9 10)))
             (princ (comm:send-message socket data (length data) nil nil :max-buffer-size 8))
             (format t "SOCKET: Send message: ~A~%" data)
